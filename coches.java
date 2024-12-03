05411a3b7580e0f68014e14b6b0c03fed7a88935
Pablo <pablo@gmail.com>
2024-12-03 20:47:10 +0100

    Comenzando cocher.java

--- coches.java
+++ coches.java
@@ -3,17 +3,8 @@ import java.util.ArrayList;
 public class coches {
     public static void main(String[] args) {
 
-        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<String> coches = new ArrayList<>();
 
-
-        nums.add(4);
-        nums.add(5);
-
-
-
-        for(int num:nums){
-            System.out.println(num);
-        }
 
 
 
