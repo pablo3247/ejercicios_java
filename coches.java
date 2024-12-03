f3eb7d4559848c6e6bf14272471f4f329e9e5ca7
Pablo <pablo@gmail.com>
2024-12-03 20:14:50 +0100

    Primer contacto!

--- coches.java
+++ coches.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class coches {
     public static void main(String[] args) {
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+
+        nums.add(4);
+        nums.add(5);
+
+
+
+        for(int num:nums){
+            System.out.println(num);
+        }
+
+
+
+
 
 
     }
