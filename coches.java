324f880f37199ead8a7c95540b0db402b3876b29
pablo_casa <pabmoranc@alu.edu.gva.es>
2024-12-03 23:02:30 +0100

    Acabado ejercicio coches.java

--- coches.java
+++ coches.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class coches {
     public static void main(String[] args) {
-
         ArrayList<String> coches = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        String marca = " ";
+        while(!marca.equals("")){
+            System.out.println("Dime una marca de coche!");
+            marca = sc.nextLine();
 
+            if(!marca.equals("")){
+                coches.add(marca);
+            }
+        }
 
-
-
-
-
+        for(String coche:coches){
+            System.out.println(coche);
+        }
 
     }
 }
