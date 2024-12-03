42560bb6a287ebdd9119b7b2a04921d3cc5fe027
pablo_casa <pabmoranc@alu.edu.gva.es>
2024-12-04 00:05:00 +0100

    Comenzando ejercicio repartiendoRegalos.java

---
+++ repartiendoRegalos.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class repartiendoRegalos {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> portales = new ArrayList<>();
+
+        portales.add(0);
+        portales.add(2);
+        portales.add(4);
+
+        ordenPortales(portales,portales.getFirst());
+    }
+
+    public static void ordenPortales(ArrayList<Integer> portales,int aterrizaje){
+
+
+        for(int i=1;i<portales.size();i++){
+
+
+
+
+        }
+
+    }
+}
