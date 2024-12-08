3982c7292c00cbc43c84b85830892ba1ab352df1
pablo <pabmoranc@alu.edu.gva.es>
2024-12-08 16:56:21 +0100

    Comenzando telefonos2

---
+++ telefonos2.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class telefonos2 {
+    public static void main(String[] args) {
+
+
+        Map<String,String> agenda = new HashMap<String,String>();
+
+
+
+    }
+}
